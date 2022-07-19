f39e96da07f7cae555d3aa9a2fdfccac796d262e
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-07-19 23:43:35 +0300

    Solution

--- user/views.py
+++ user/views.py
@@ -13,10 +13,6 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class CreateTokenView(ObtainAuthToken):
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-
-
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     authentication_classes = (TokenAuthentication,)
