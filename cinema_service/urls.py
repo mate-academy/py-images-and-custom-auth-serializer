c7f954b895922e01745b4a2f6b9bd5de87016518
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-07-19 17:22:59 +0300

    Fix

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -9,3 +9,4 @@ urlpatterns = [
     path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#comment
