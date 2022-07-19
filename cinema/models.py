821367569bf9f7874a187884dcf2c86731e713e6
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-07-19 16:33:18 +0300

    fix_problems

--- cinema/models.py
+++ cinema/models.py
@@ -75,7 +75,10 @@ class MovieSession(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return str(self.created_at)
@@ -88,7 +91,10 @@ class Ticket(models.Model):
     movie_session = models.ForeignKey(
         MovieSession, on_delete=models.CASCADE, related_name="tickets"
     )
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE,
+        related_name="tickets"
+    )
     row = models.IntegerField()
     seat = models.IntegerField()
 
--- cinema/serializers.py
+++ cinema/serializers.py
@@ -45,9 +45,13 @@ class MovieListSerializer(MovieSerializer):
         fields = ("id", "title", "description", "duration", "genres", "actors", "image")
 
 
-class MovieDetailSerializer(MovieListSerializer):
+class MovieDetailSerializer(MovieSerializer):
     genres = GenreSerializer(many=True, read_only=True)
     actors = ActorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = ("id", "title", "description", "duration", "genres", "actors", "image")
 
 
 class MovieSessionSerializer(serializers.ModelSerializer):
