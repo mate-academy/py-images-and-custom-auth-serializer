2d2ffdefa62aa12e13f1d390ade1bc6203d62144
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-07-21 12:14:51 +0300

    Fiq quotes

--- user/serializers.py
+++ user/serializers.py
@@ -30,28 +30,28 @@ class UserSerializer(serializers.ModelSerializer):
 class AuthTokenSerializer(serializers.Serializer):
     email = serializers.EmailField(label=_("Email"))
     password = serializers.CharField(
-        label=_("Password", ),
-        style={'input_type': 'password'},
-        trim_whitespace=False
+        label=_(
+            "Password",
+        ),
+        style={"input_type": "password"},
+        trim_whitespace=False,
     )
 
     def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
+        email = attrs.get("email")
+        password = attrs.get("password")
 
         if email and password:
-            user = authenticate(request=self.context.get('request'),
-                                email=email, password=password)
+            user = authenticate(
+                request=self.context.get("request"), email=email, password=password
+            )
 
-            # The authenticate call simply returns None for is_active=False
-            # users. (Assuming the default ModelBackend authentication
-            # backend.)
             if not user:
-                msg = _('Unable to log in with provided credentials.')
-                raise serializers.ValidationError(msg, code='authorization')
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
         else:
             msg = _('Must include "username" and "password".')
-            raise serializers.ValidationError(msg, code='authorization')
+            raise serializers.ValidationError(msg, code="authorization")
 
-        attrs['user'] = user
+        attrs["user"] = user
         return attrs
