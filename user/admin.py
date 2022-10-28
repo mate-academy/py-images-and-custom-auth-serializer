aaf336d13b5f5b41b5c81f52d9260e42db06d614
Sergii Kantor <sergiy.kantor@gmail.com>
2022-10-28 16:57:07 +0300

    fixed all finally)

--- user/admin.py
+++ user/admin.py
@@ -22,6 +22,6 @@ class UserAdmin(DjangoUserAdmin):
             "fields": ("email", "password1", "password2"),
         }),
     )
-    list_display = ("emai", "first_name", "last_name", "is_staff")
+    list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
