70d1f2b78f38e51f01bf62b3cfe3054d48e6aba3
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-23 18:15:39 +0200

    Black style

--- user/models.py
+++ user/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext as _
