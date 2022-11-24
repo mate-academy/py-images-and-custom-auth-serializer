240b7719cd69427d8f1158ecf8ee663d8ef97441
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-24 15:34:29 +0200

    2 try

--- user/views.py
+++ user/views.py
@@ -7,7 +7,6 @@ from rest_framework.settings import api_settings
 from user.serializers import UserSerializer
 from user.serializers import AuthTokenSerializer
 
-
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
