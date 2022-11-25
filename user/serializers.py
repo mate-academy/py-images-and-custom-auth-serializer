93d39a54aef01672cd5aed432ee1c8995b4ff010
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-25 15:56:11 +0200

    serializer fixes

--- user/serializers.py
+++ user/serializers.py
@@ -25,15 +25,21 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 
-class AuthByEmailTokenSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
-
-    def create(self, validated_data):
-        pass
-
-    def update(self, instance, validated_data):
-        pass
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(
+        label=_("Email"),
+        write_only=True
+    )
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+    token = serializers.CharField(
+        label=_("Token"),
+        read_only=True
+    )
 
     def validate(self, attrs):
         email = attrs.get("email")
