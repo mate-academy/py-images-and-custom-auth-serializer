fd78d1663eea71b10e50b1648c7b1f6c0454b740
Yurii <pazyn404@gmail.com>
2022-12-05 10:51:44 +0200

    Fix solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -42,7 +42,7 @@ class MovieSerializer(serializers.ModelSerializer):
             "actors",
             "image",
         )
-        read_only_fields = ("image",)
+        read_only_fields = ("id", "image", )
 
 
 class MovieListSerializer(MovieSerializer):
--- cinema/views.py
+++ cinema/views.py
@@ -121,7 +121,7 @@ class MovieViewSet(
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
