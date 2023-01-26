eb1f4c92943c28d1a9414ec5f4ab5fe9881b777c
Vlad Hrytsai <sunrises2929@gmail.com>
2023-01-26 13:06:01 +0200

    solution

--- user/serializers.py
+++ user/serializers.py
@@ -1,24 +1,52 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.core import exceptions
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = get_user_model()
         fields = ("id", "email", "password", "is_staff")
-        read_only_fields = ("is_staff",)
+        read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
-        """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
-        """Update a user, set the password correctly and return it"""
         password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
+
         if password:
             user.set_password(password)
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(style={"input_type": "password"})
+
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+
+        if email and password:
+            user = authenticate(email=email, password=password)
+
+            if user:
+                if not user.is_active:
+                    msg = _("User account is disabled.")
+                    raise exceptions.ValidationError(msg)
+            else:
+                msg = _("Unable to log in with provided credentials.")
+                raise exceptions.ValidationError(msg)
+        else:
+            msg = _("Must include 'email' and 'password'.")
+            raise exceptions.ValidationError(msg)
+
+        data["user"] = user
+        return data
