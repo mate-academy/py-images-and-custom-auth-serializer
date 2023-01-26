c6001c8b79fa4bd442bd04a9cbbe041eefbb55ec
Vlad Hrytsai <sunrises2929@gmail.com>
2023-01-26 13:29:16 +0200

    solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -110,8 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME":
-            "django.contrib.auth.password_validation." 
-            "NumericPasswordValidator",
+            "django.contrib.auth.password_validation." "NumericPasswordValidator",
     },
 ]
 
