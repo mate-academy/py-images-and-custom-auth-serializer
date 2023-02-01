a8fe68c30e03aadec62713004128dc7a7d15f0a0
Vladyslav Hrytsai <98831872+HrytVlad@users.noreply.github.com>
2023-02-01 14:41:40 +0200

    Update views.py

--- cinema/views.py
+++ cinema/views.py
@@ -104,7 +104,7 @@ class MovieViewSet(
         if self.action == "retrieve":
             return MovieDetailSerializer
 
-        if self.action == "upload-image":
+        if self.action == "upload_image":
             return MovieImageSerializer
 
         return MovieSerializer
