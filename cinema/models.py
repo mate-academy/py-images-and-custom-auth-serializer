86c0a94a235467a9faef754ff3f9cec05fc9fdd6
Vladyslav Hrytsai <98831872+HrytVlad@users.noreply.github.com>
2023-02-01 14:22:52 +0200

    Update models.py
    add type annotations

--- cinema/models.py
+++ cinema/models.py
@@ -39,7 +39,7 @@ class Actor(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
-def movie_image_file_path(instance, filename):
+def movie_image_file_path(instance: str, filename: str):
     _, extension = os.path.splitext(filename)
 
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
