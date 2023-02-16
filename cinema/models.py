a5e37492a3a0f7d6861509ab3fe3c92b575f54bf
Anton Chumak <Chumak7377@gmail.com>
2023-02-16 09:09:57 +0200

    Add ImageField image to the Movie model & def movie_image_file_path

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,10 @@
+import os
+import uuid
+
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class CinemaHall(models.Model):
@@ -28,11 +32,19 @@ class Actor(models.Model):
     last_name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return self.full_name
 
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+
+def movie_image_file_path(movie, filename):
+    _, extension = os.path.splitext(filename)
+
+    filename = f"{slugify(movie.title)}-{uuid.uuid4()}.{extension}"
+
+    return os.path.join("upload-image/", filename)
 
 
 class Movie(models.Model):
@@ -41,6 +53,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
+    image = models.ImageField(null=True, upload_to=movie_image_file_path)
 
     class Meta:
         ordering = ["title"]
