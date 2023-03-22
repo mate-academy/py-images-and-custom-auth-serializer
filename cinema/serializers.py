033c297b1695f0d46fdf11c9b94a507694ba960c
Ivan Mayor <maramorosh2@gmail.com>
2023-03-22 15:53:04 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -66,7 +66,7 @@ class MovieDetailSerializer(MovieSerializer):
 
     class Meta:
         model = Movie
-        fields = ("id", "title", "description", "duration", "genres", "actors")
+        fields = ("id", "title", "description", "duration", "genres", "actors", "image")
 
 
 class MovieSessionSerializer(serializers.ModelSerializer):
