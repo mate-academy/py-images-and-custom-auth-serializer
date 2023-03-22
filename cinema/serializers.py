7867574f7c16d776084bbe67ad2a643e881c89a7
Ivan Mayor <maramorosh2@gmail.com>
2023-03-22 15:50:25 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -84,7 +84,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
         source="cinema_hall.capacity", read_only=True
     )
     tickets_available = serializers.IntegerField(read_only=True)
-    image = serializers.ImageField(source="movie.image", read_only=True)
+    movie_image = serializers.ImageField(source="movie.image", read_only=True)
 
     class Meta:
         model = MovieSession
@@ -95,7 +95,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
             "cinema_hall_name",
             "cinema_hall_capacity",
             "tickets_available",
-            "image"
+            "movie_image"
         )
 
 
