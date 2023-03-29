50f6fd6b1ef9b732fefdc7f9fa35c89168a099d9
Tania Kharchuk <tkharchuk14@gmail.com>
2023-03-29 22:10:29 +0300

    Fix flake8

--- user/serializers.py
+++ user/serializers.py
@@ -29,23 +29,23 @@ class AuthTokenSerializer(serializers.Serializer):
     email = serializers.EmailField(label=_("Email"))
     password = serializers.CharField(
         label=_("Password", ),
-        style={'input_type': 'password'},
+        style={"input_type": "password"},
         trim_whitespace=False
     )
 
     def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
+        email = attrs.get("email")
+        password = attrs.get("password")
 
         if email and password:
-            user = authenticate(request=self.context.get('request'),
+            user = authenticate(request=self.context.get("request"),
                                 email=email, password=password)
             if not user:
-                msg = _('Unable to log in with provided credentials.')
-                raise serializers.ValidationError(msg, code='authorization')
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
         else:
-            msg = _('Must include "email" and "password".')
-            raise serializers.ValidationError(msg, code='authorization')
+            msg = _("Must include email and password.")
+            raise serializers.ValidationError(msg, code="authorization")
 
-        attrs['user'] = user
+        attrs["user"] = user
         return attrs
