38b2107f89c727935e009285db0ee7fa97c26eda
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-04-01 09:50:06 +0300

    Changed path to images

--- cinema/models.py
+++ cinema/models.py
@@ -43,7 +43,7 @@ def movie_image_file_path(instance, filename) -> str:
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/courses/", filename)
+    return os.path.join("uploads/images/", filename)
 
 
 class Movie(models.Model):
