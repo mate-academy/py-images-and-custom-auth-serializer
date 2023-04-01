4fb731f990a3a0964688b654e56add119b676fb7
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-04-01 10:38:03 +0300

    Changed path ещ ьto media root in settings

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,7 +135,7 @@ STATIC_URL = "static/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 
-MEDIA_ROOT = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,'/media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
