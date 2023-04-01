a853067b89f2d5f03f1114545240c175324c3408
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-04-01 10:40:33 +0300

    Changed path ещ ьto media root in settings

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = "static/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 
-MEDIA_URL = os.path.join(BASE_DIR, '/media/')
+MEDIA_URL = os.path.join(BASE_DIR, "/media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
