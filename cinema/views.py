e2671321eb6cb83ddb48a835035be6772dd269e5
Danylo <danildacenko@gmail.com>
2023-04-12 01:47:09 +0300

    remove useless import

--- cinema/views.py
+++ cinema/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
