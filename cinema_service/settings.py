6668e118891cca83dc65a6cb018e02d5ec450367
Artur Tytomyr <art.tytomyr@gmail.com>
2023-04-12 08:56:10 +0300

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -125,7 +125,7 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
@@ -133,6 +133,9 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
+
+MEDIA_ROOT = BASE_DIR / " media"
+MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
