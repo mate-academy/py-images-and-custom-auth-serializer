f0aacbe5b4527d8f432fb2593ace1983ec90999c
Mykhailo Vovchok <mivvaw@gmail.com>
2023-04-17 14:53:22 +0300

    simplified

--- user/serializers.py
+++ user/serializers.py
@@ -28,7 +28,8 @@ class UserSerializer(serializers.ModelSerializer):
 class AuthTokenSerializer(serializers.Serializer):
     email = serializers.CharField(label=_("Email"))
     password = serializers.CharField(
-        label=_("Password"), style={"input_type": "password"}
+        label=_("Password"),
+        style={"input_type": "password"}
     )
 
     def validate(self, attrs):
@@ -38,17 +39,11 @@ class AuthTokenSerializer(serializers.Serializer):
         if email and password:
             user = authenticate(email=email, password=password)
 
-            if user:
-                if not user.is_active:
-                    msg = _("User account is disabled.")
-                    raise serializers.ValidationError(
-                        msg, code="authorization"
-                    )
-            else:
+            if not user:
                 msg = _("Unable to log in with provided credentials.")
                 raise serializers.ValidationError(msg, code="authorization")
         else:
-            msg = _("Must include 'username' and 'password'.")
+            msg = _("Must include email and password.")
             raise serializers.ValidationError(msg, code="authorization")
 
         attrs["user"] = user
