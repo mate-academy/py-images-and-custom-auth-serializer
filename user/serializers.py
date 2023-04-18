d4c5a77838a58367bace80e05b7e3d2bc7cc789f
Maksym Pyshniak <pyshniakm91@gmail.com>
2023-04-18 17:51:29 +0300

    v1

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -22,3 +23,28 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={"input_type": "password"}, trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            request=self.context.get("request"),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError(msg, code="authentication")
+
+        attrs["user"] = user
+        return attrs
