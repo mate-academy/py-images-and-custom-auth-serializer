d968fb27b45d8646c1af92c3ec48afc1fe2317cd
Mykola Savchyn <kola.savchyn@gmail.com>
2023-04-20 19:20:31 +0300

    'Solution'

--- cinema/views.py
+++ cinema/views.py
@@ -111,7 +111,7 @@ class MovieViewSet(
     @action(methods=["POST"],
             detail=True,
             url_path="upload-image",
-            permission_classes=IsAdminUser)
+            )
     def upload_image(self, request, pk=None):
 
         movie = self.get_object()
