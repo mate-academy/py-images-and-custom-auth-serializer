42d0191a678b9cb144496d816f9e4c2c26180d33
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-05-03 12:05:31 +0300

    Solution

--- user/serializers.py
+++ user/serializers.py
@@ -39,7 +39,7 @@ class AuthTokenSerializer(serializers.Serializer):
         if email and password:
             user = authenticate(
                 request=self.context.get("request"),
-                username=email,
+                email=email,
                 password=password
             )
             if not user:
