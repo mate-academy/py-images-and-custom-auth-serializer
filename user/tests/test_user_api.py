675f0004dc732a5faee8f0f3ffa6eaad994d2f0e
kostya-kononenko <kkononenko3@gmail.com>
2023-06-03 12:39:33 +0300

    Solution

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -56,9 +56,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = (
-            get_user_model().objects.filter(email=payload["email"]).exists()
-        )
+        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
