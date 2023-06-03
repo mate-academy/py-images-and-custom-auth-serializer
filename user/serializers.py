5f65ec7e2da8d9d0645527c655e9063f0a3de565
kostya-kononenko <kkononenko3@gmail.com>
2023-06-03 12:53:35 +0300

    Solution2

--- user/serializers.py
+++ user/serializers.py
@@ -50,7 +50,7 @@ class AuthTokenSerializer(serializers.Serializer):
                 msg = _("Unable to log in with provided credentials.")
                 raise serializers.ValidationError(msg, code="authorization")
         else:
-            msg = _('Must include "username" and "password".')
+            msg = _('Must include "email" and "password".')
             raise serializers.ValidationError(msg, code="authorization")
 
         attrs["user"] = user
