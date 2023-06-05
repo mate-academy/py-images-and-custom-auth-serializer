b85889b44b5e7efdd2c998721f77c3292f01dce6
Amirammi <amirammina@gmail.com>
2023-06-05 16:56:32 +0300

    Solution

--- user/serializers.py
+++ user/serializers.py
@@ -1,24 +1,50 @@
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers, exceptions
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("id", "email", "password", "is_staff")
-        read_only_fields = ("is_staff",)
+        read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
-        """Create a new user with encrypted password and return it"""
+        """Create user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
-        """Update a user, set the password correctly and return it"""
+        """Update user with correctly encrypted password"""
         password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
+
         if password:
             user.set_password(password)
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(style={"input_type": "password"})
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = authenticate(email=email, password=password)
+
+            if not user:
+                raise exceptions.ValidationError(
+                    "Unable to login with provided credentials"
+                )
+
+            attrs["user"] = user
+        else:
+            raise exceptions.ValidationError(
+                "Please, provide email and password"
+            )
+
+        return attrs
