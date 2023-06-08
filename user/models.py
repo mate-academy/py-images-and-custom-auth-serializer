2b2cde6bdcb2708a5448fae8d9573062bcaccafe
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-08 12:23:33 +0300

    fix model User with REQUIRED_FIELDS & fix USERNAME_FIELD problem with list

--- user/models.py
+++ user/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
