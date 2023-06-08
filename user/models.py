5d82252dde480a74bc66c5749091460f5892bc6b
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-08 14:10:10 +0300

    fix indents in UserManager

--- user/models.py
+++ user/models.py
@@ -19,24 +19,22 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
 
-def create_user(self, email, password=None, **extra_fields):
-    """Create and save a regular User with the given email and password."""
-    extra_fields.setdefault("is_staff", False)
-    extra_fields.setdefault("is_superuser", False)
-    return self._create_user(email, password, **extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-
-def create_superuser(self, email, password, **extra_fields):
-    """Create and save a SuperUser with the given email and password."""
-    extra_fields.setdefault("is_staff", True)
-    extra_fields.setdefault("is_superuser", True)
-
-    if extra_fields.get("is_staff") is not True:
-        raise ValueError("Superuser must have is_staff=True.")
-    if extra_fields.get("is_superuser") is not True:
-        raise ValueError("Superuser must have is_superuser=True.")
-    return self._create_user(email, password, **extra_fields)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
