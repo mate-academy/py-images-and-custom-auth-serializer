9e0164774d437a87d72cb50d6317dae5dbb5e811
OleksandrYanchuk <sashajanchuck13@gmail.com>
2023-07-01 21:38:07 +0300

    'Solution'

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -3,11 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-                  path("admin/", admin.site.urls),
-                  path(
-                      "api/cinema/", include("cinema.urls", namespace="cinema")
-                  ),
-                  path("api/user/", include("user.urls", namespace="user")),
-                  path("__debug__/", include("debug_toolbar.urls")),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+    path("api/cinema/", include("cinema.urls", namespace="cinema")),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
