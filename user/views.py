54eee2155304b95577f92df4db48f0300c01f2a2
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-02 19:55:15 +0200

    "solution"

--- user/views.py
+++ user/views.py
@@ -1,10 +1,15 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.schemas import ManualSchema
+from rest_framework.compat import coreapi, coreschema
+from rest_framework.schemas import coreapi as coreapi_schema
 from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -13,6 +18,35 @@ class CreateUserView(generics.CreateAPIView):
 
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
+
+    if coreapi_schema.is_enabled():
+        schema = ManualSchema(
+            fields=[
+                coreapi.Field(
+                    name="email",
+                    required=True,
+                    location="form",
+                    schema=coreschema.String(
+                        title="Email",
+                        description="Valid username for authentication",
+                    ),
+                ),
+                coreapi.Field(
+                    name="password",
+                    required=True,
+                    location="form",
+                    schema=coreschema.String(
+                        title="Password",
+                        description="Valid password for authentication",
+                    ),
+                ),
+            ],
+            encoding="application/json",
+        )
+
+
+obtain_auth_token = ObtainAuthToken.as_view()
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
