85148e2339a4f67062e6aa9d4712d7f9af3bae33
Petro Abramov <sebshe3@gmail.com>
2023-07-06 13:34:28 +0300

    Changed join in models.py

--- cinema/models.py
+++ cinema/models.py
@@ -44,7 +44,7 @@ def movie_image_file_path(instance, filename):
 
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{ext}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads", "movies", filename)
 
 
 class Movie(models.Model):
