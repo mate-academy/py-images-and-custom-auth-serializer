0e04f4046d1ee24bee19b6ebcaa9f183976ebe72
Volodymyr Bohomol <8773927@gmail.com>
2023-08-02 16:44:58 +0300

    Fixed serializer validation `upload_image` func

--- cinema/views.py
+++ cinema/views.py
@@ -119,11 +119,9 @@ class MovieViewSet(
         movie = self.get_object()
         serializer = self.get_serializer(movie, data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
