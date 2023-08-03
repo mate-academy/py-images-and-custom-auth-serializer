4f250939b1ccba599581f1b7b59b41c923f37b17
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-03 15:41:57 +0300

    unnecessary comment from custom token serializer removed, validation message of token serializer changed

--- user/serializers.py
+++ user/serializers.py
@@ -48,14 +48,11 @@ class AuthTokenSerializerCustom(serializers.Serializer):
                 password=password,
             )
 
-            # The authenticate call simply returns None for is_active=False
-            # users. (Assuming the default ModelBackend authentication
-            # backend.)
             if not user:
                 msg = _("Unable to log in with provided credentials.")
                 raise serializers.ValidationError(msg, code="authorization")
         else:
-            msg = _('Must include "username" and "password".')
+            msg = _('Must include "email" and "password".')
             raise serializers.ValidationError(msg, code="authorization")
 
         attrs["user"] = user
