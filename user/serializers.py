967ed55e91854058d238cc9c5ad0316ec5924544
YOUR_NAME <YOUR_EMAIL>
2023-08-03 21:23:24 +0300

    Images and custom auth token serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -22,3 +23,28 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = get_user_model().objects.filter(email=email).first()
+
+            if user and user.check_password(password):
+                attrs["user"] = user
+                return attrs
+
+        raise serializers.ValidationError(
+            "Invalid email or password."
+        )
+
+    def create(self, validated_data):
+        user = validated_data["user"]
+        token, _ = Token.objects.get_or_create(user=user)
+        return token
