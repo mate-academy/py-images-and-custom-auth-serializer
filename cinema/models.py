b477b3904db787d6d289590b0918c892888b7090
Eugene Polunin <altahoma@gmail.com>
2023-08-03 23:53:00 +0300

    Fixed flake8 error - E501 line too long

--- cinema/models.py
+++ cinema/models.py
@@ -43,7 +43,7 @@ def create_custom_path(instance, filename):
     _, extension = os.path.splitext(filename)
     return os.path.join(
         "uploads/images/",
-        f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+        f"{slugify(instance.title)}-{uuid.uuid4()}{extension}",
     )
 
 
@@ -108,9 +108,9 @@ class Ticket(models.Model):
                 raise error_to_raise(
                     {
                         ticket_attr_name: f"{ticket_attr_name} "
-                                          f"number must be in available range: "
-                                          f"(1, {cinema_hall_attr_name}): "
-                                          f"(1, {count_attrs})"
+                        f"number must be in available range: "
+                        f"(1, {cinema_hall_attr_name}): "
+                        f"(1, {count_attrs})"
                     }
                 )
 
@@ -123,11 +123,11 @@ class Ticket(models.Model):
         )
 
     def save(
-            self,
-            force_insert=False,
-            force_update=False,
-            using=None,
-            update_fields=None,
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
     ):
         self.full_clean()
         return super(Ticket, self).save(
