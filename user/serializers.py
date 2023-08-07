51893fd00a8a69ed0582af55b5f7e27981dcdd46
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-08-07 18:40:20 +0300

    Implement AuthTokenSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -22,3 +23,40 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(
+        label=_("Email"),
+        write_only=True,
+    )
+    password = serializers.CharField(
+        label=_("Password"),
+        write_only=True,
+        style={"input_type": "password"}
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = authenticate(
+                request=self.context.get("request"),
+                email=email,
+                password=password,
+            )
+
+            if not user:
+                raise serializers.ValidationError(
+                    _("Unable to log in with provided credentials."),
+                    code="authorization",
+                )
+        else:
+            raise serializers.ValidationError(
+                _('Must include "email" and "password".'),
+                code="authorization",
+            )
+
+        attrs["user"] = user
+        return attrs
