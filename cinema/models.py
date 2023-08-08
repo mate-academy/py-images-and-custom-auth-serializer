24bdaa6ee48df22198f90466c9f93290d1bcb7fc
Bohdan Liutko <luitko007@gmail.com>
2023-08-08 21:09:47 +0000

    import fix in models

--- cinema/models.py
+++ cinema/models.py
@@ -1,11 +1,10 @@
+import os.path
+import uuid
+
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.text import slugify
-
-
-import os.path
-import uuid
 
 
 class CinemaHall(models.Model):
