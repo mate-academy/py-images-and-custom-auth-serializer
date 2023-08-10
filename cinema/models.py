4aca33383c54b42669c9b9a4186d68dab8904792
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-08-10 19:07:06 +0300

    change function that creates image file path

--- cinema/models.py
+++ cinema/models.py
@@ -40,9 +40,9 @@ class Actor(models.Model):
 
 def movie_image_file_path(instance, filename) -> str:
     _, ext = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}-{uuid.uuid4()}.{ext}"
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{ext}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads", "movies", filename)
 
 
 class Movie(models.Model):
