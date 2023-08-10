c342185367acf0cedd62ed4409f8bf11ae2df5c2
Illia Smyrnov <ilushasm@gmail.com>
2023-08-10 14:47:00 +0300

    Removed if statement in upload_image action

--- cinema/models.py
+++ cinema/models.py
@@ -43,7 +43,7 @@ def movie_image_file_path(instance, filename) -> str:
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("images/buses/", filename)
+    return os.path.join("uploads/movies/", filename)
 
 
 class Movie(models.Model):
