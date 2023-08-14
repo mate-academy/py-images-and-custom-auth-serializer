79c44df4c34e288c7fd05ab8d0fefea71ba61226
Dmitry Iorgansky <71707019+Dimdimior@users.noreply.github.com>
2023-08-14 14:35:18 +0300

    fix

--- user/models.py
+++ user/models.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractUser
 from django.db import models
 from django.utils.translation import gettext as _
 
