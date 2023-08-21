7349685071777cd61645f1b0ceaeb5a618baf46b
Alexandr Nikiforenko <NikiforenkoAS@i.ua>
2023-08-21 20:33:48 +0300

    Solution

--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
@@ -22,3 +23,35 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            request=self.context.get("request"),
+            username=email,
+            password=password
+        )
+
+        if not user:
+            msg = _("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError(msg, code="authorization")
+
+        if not user.is_active:
+            msg = _(
+                "The password is valid, but the account has been disabled!"
+            )
+            raise serializers.ValidationError(msg, code="not_active")
+
+        attrs["user"] = user
+        attrs["is_staff"] = user.is_staff
+        return attrs
