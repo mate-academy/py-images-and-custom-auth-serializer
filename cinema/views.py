7a55a1f4521bfa9272294b8e7fdc54b4b96d52cc
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-09-05 10:59:03 +0300

    Replaced if-statement

--- cinema/views.py
+++ cinema/views.py
@@ -118,10 +118,9 @@ class MovieViewSet(
     def upload_image(self, request, pk=None):
         movie = self.get_object()
         serializer = self.get_serializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
