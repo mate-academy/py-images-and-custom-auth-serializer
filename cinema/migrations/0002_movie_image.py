6abfd34dfdc78e88103c25f47c16f9dadc929a17
Nataliia Podoroha <ingenuous999@gmail.com>
2023-09-07 14:47:00 +0200

    Solution_with_black

--- cinema/migrations/0002_movie_image.py
+++ cinema/migrations/0002_movie_image.py
@@ -5,15 +5,16 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('cinema', '0001_initial'),
+        ("cinema", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='movie',
-            name='image',
-            field=models.ImageField(null=True, upload_to=cinema.models.movie_image_file_path),
+            model_name="movie",
+            name="image",
+            field=models.ImageField(
+                null=True, upload_to=cinema.models.movie_image_file_path
+            ),
         ),
     ]
