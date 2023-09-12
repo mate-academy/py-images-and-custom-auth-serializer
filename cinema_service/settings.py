c533bbdba1eba18b8cf1fe1d7c5a8b38ffe6fd38
Taras Savchyn <savik1992@gmail.com>
2023-09-12 22:58:50 +0300

    use email instead of username

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -114,7 +114,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "user.User"
+# User substitution
+# https://docs.djangoproject.com/en/1.11/topics/auth/customizing/#auth-custom-user
+
+AUTH_USER_MODEL = 'user.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
