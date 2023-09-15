cf66b549304cd0609bc2eff39f5bfcb0b2c79ae1
Taras Savchyn <savik1992@gmail.com>
2023-09-16 00:51:24 +0300

    delete surplus imports

--- user/views.py
+++ user/views.py
@@ -2,9 +2,7 @@ from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.response import Response
 from rest_framework.settings import api_settings
-from rest_framework.authtoken.models import Token
 
 from user.serializers import UserSerializer, CustomAuthTokenSerializer
 
