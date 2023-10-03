3a0cbefd87345f43b8f06018edfc508cc255fb09
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-03 19:37:18 +0300

    Create Custom UserManager for User model

--- user/models.py
+++ user/models.py
@@ -1,6 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext as _
+
+
+class UserManager(BaseUserManager):
+    user_in_migrations = True
+
+    def _create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, password=None, **kwargs):
+        kwargs.setdefault("is_staff", False)
+        kwargs.setdefault("is_super_user", False)
+
+        return self._create_user(email, password, **kwargs)
+
+    def create_superuser(self, email, password=None, **kwargs):
+        kwargs.setdefault("is_super_user", True)
+        kwargs.setdefault("is_staff", True)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **kwargs)
 
 
 class User(AbstractUser):
