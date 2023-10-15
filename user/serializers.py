e5d88f2622101c3ed4db099d964c8c7a3fe9a876
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-10-16 00:56:57 +0300

    Fix code style

--- user/serializers.py
+++ user/serializers.py
@@ -46,14 +46,11 @@ class AuthTokenSerializer(serializers.Serializer):
                 password=password
             )
 
-            # The authenticate call simply returns None for is_active=False
-            # users. (Assuming the default ModelBackend authentication
-            # backend.)
             if not user:
                 msg = _("Unable to log in with provided credentials.")
                 raise serializers.ValidationError(msg, code="authorization")
         else:
-            msg = _('Must include "email" and "password".')
+            msg = _("Must include 'email' and 'password'.")
             raise serializers.ValidationError(msg, code="authorization")
 
         attrs["user"] = user
