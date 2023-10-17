b2e3fb004189aa50b2ee7833d04435de19d15c02
ORENYT <106102798+ORENYT@users.noreply.github.com>
2023-10-17 19:49:21 +0300

    Removed one state in permission

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -1,13 +1,6 @@
-from rest_framework.permissions import SAFE_METHODS, BasePermission
+from rest_framework.permissions import BasePermission
 
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            (
-                request.method in SAFE_METHODS
-                and request.user
-                and request.user.is_authenticated
-            )
-            or (request.user and request.user.is_staff)
-        )
+        return bool((request.user and request.user.is_staff))
