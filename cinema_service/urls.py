10ac0a256cb9bc20169c27c31bbf891d2af48607
Andrii Kurta <andriykurta97@gmail,com>
2023-10-17 22:16:52 +0200

    Fix flake8

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -9,4 +9,3 @@ urlpatterns = [
     path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- user/serializers.py
+++ user/serializers.py
@@ -32,7 +32,7 @@ class AuthTokenSerializer(serializers.Serializer):
     )
     password = serializers.CharField(
         label=_("Password"),
-        style={'input_type': 'password'},
+        style={"input_type": "password"},
         trim_whitespace=False,
         write_only=True
     )
@@ -42,19 +42,23 @@ class AuthTokenSerializer(serializers.Serializer):
     )
 
     def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
+        email = attrs.get("email")
+        password = attrs.get("password")
 
         if email and password:
-            user = authenticate(request=self.context.get('request'),
-                                email=email, password=password)
+            user = authenticate(
+                request=self.context.get("request"),
+                email=email, password=password
+            )
 
             if not user:
-                msg = _('Unable to log in with provided credentials.')
-                raise serializers.ValidationError(msg, code='authorization')
-        else:
-            msg = _('Must include "username" and "password".')
-            raise serializers.ValidationError(msg, code='authorization')
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
 
-        attrs['user'] = user
+        else:
+            msg = _("Must include 'email' and 'password'.")
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+
         return attrs
