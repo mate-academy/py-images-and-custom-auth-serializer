6e6702800a560b752425456d428e5e6fcf21c44e
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-10-18 23:31:35 +0300

    'Solution'

--- user/models.py
+++ user/models.py
@@ -8,6 +8,7 @@ from django.utils.translation import gettext as _
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
+
     def _create_user(self, email, password, **extra_fields):
         """Create and save a User with the given email and password."""
         if not email:
