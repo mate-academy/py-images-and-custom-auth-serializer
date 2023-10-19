76af52d66c650c44ccb7efeed3851927c67903de
moroz_olexandr <oleksandrmoroz2003@gmail.com>
2023-10-19 18:06:51 +0300

    all task

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,16 @@
+import os
+import uuid
+
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
+
+
+def posters(instance, filename):
+    _, ext = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}.{ext}"
+    return os.path.join("uploads/movies/", filename)
 
 
 class CinemaHall(models.Model):
@@ -41,6 +51,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
+    image = models.ImageField(upload_to=posters, null=True)
 
     class Meta:
         ordering = ["title"]
