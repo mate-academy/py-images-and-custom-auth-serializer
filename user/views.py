d793131e6095ab5f2e53a5f5871385086523133a
Mazur Mariya <mazurinka29@gmail.com>
2023-10-22 11:28:52 +0300

    Solution py-images-and-custom-auth-serializer

--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
+
 from rest_framework.authentication import TokenAuthentication
+
 from rest_framework.permissions import IsAuthenticated
+
 from rest_framework.authtoken.views import ObtainAuthToken
 
 from rest_framework.settings import api_settings
