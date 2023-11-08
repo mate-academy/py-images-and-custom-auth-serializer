42ab59eebddc8679fa7461adcc58d8585d987ac5
Davydova Olha <o.chookity@gmail.com>
2023-11-08 17:44:45 +0200

    Fix flake8

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -39,7 +39,15 @@ class MovieImageSerializer(serializers.ModelSerializer):
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ("id", "title", "description", "duration", "genres", "actors", "image")
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors",
+            "image"
+        )
         read_only_fields = ("image",)
 
 
@@ -58,7 +66,15 @@ class MovieDetailSerializer(MovieSerializer):
 
     class Meta:
         model = Movie
-        fields = ("id", "title", "description", "duration", "genres", "actors", "image")
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors",
+            "image"
+        )
 
 
 class MovieSessionSerializer(serializers.ModelSerializer):
