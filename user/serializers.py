ceeaf713bd6d8199bf0026a8be9b554f9b4a1104
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-12-05 23:25:24 +0200

    Updated requirements.txt

--- user/serializers.py
+++ user/serializers.py
@@ -47,9 +47,6 @@ class AuthTokenSerializer(serializers.Serializer):
                 password=password
             )
 
-            # The authenticate call simply returns None for is_active=False
-            # users. (Assuming the default ModelBackend authentication
-            # backend.)
             if not user:
                 msg = _("Unable to log in with provided credentials.")
                 raise serializers.ValidationError(msg, code="authorization")
