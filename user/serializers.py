db1e5fcb6c6486f6e06cdf0271ce9bc04d373d8c
Ihor Prochnickiy <2prochigor@gmail.com>
2023-12-05 13:26:36 +0200

    add transaction to user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model, authenticate
+from django.db import transaction
 from rest_framework import serializers
 from django.utils.translation import gettext as _
 
@@ -11,18 +12,17 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
-        """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
-        """Update a user, set the password correctly and return it"""
-        password = validated_data.pop("password", None)
-        user = super().update(instance, validated_data)
-        if password:
-            user.set_password(password)
-            user.save()
+        with transaction.atomic():
+            password = validated_data.pop("password", None)
+            user = super().update(instance, validated_data)
+            if password:
+                user.set_password(password)
+                user.save()
 
-        return user
+            return user
 
 
 class MyAuthTokenSerializer(serializers.Serializer):
