d277450d98a9126dd850a933647294c0c2a3da67
Ihor Prochnickiy <2prochigor@gmail.com>
2023-12-07 18:01:30 +0200

    remove imports in views

--- user/views.py
+++ user/views.py
@@ -2,9 +2,6 @@ from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
-from rest_framework.authtoken import views as auth_views
-from rest_framework.compat import coreapi, coreschema
-from rest_framework.schemas import ManualSchema
 from rest_framework.authtoken.views import ObtainAuthToken
 
 from user.serializers import UserSerializer
