e8ca0af7916ea6b276bab452cbd06f0bc241d56a
Yaroslav Rybalka <60896665+markus2812@users.noreply.github.com>
2023-12-13 15:42:03 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -118,9 +118,9 @@ class MovieViewSet(
         movie = self.get_object()
         serializer = self.get_serializer(movie, data=request.data)
 
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
