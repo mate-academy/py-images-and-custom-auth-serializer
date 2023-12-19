297c157ab4a31c054eb1839368ef23b287dc1284
asdadaversa <peikqudakeus@gmail.com>
2023-12-19 13:03:01 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -4,7 +4,7 @@ from django.db.models import F, Count
 from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 from rest_framework.decorators import action
