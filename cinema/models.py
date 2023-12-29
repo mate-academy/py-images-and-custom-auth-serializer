915ca7744ea3e38d6d8559ef9d19de44db0963a0
Anastasia <anastasiiya.p@gmail.com>
2023-12-29 15:02:50 +0200

    Fix os.path.join

--- cinema/models.py
+++ cinema/models.py
@@ -43,7 +43,7 @@ def movie_image_file_path(instance, filename):
     _, ext = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{ext}"
 
-    return os.path.join("uploads/movies", filename)
+    return os.path.join("uploads", "movies", filename)
 
 
 class Movie(models.Model):
