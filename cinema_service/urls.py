a7a8f81a3ae92551c69f3ecb6b43de91920d9c8d
Ivan <ivandyriv11@gmail.com>
2024-01-25 18:15:17 +0100

    Solution by Dyriv

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -6,4 +8,4 @@ urlpatterns = [
     path("api/cinema/", include("cinema.urls", namespace="cinema")),
     path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
