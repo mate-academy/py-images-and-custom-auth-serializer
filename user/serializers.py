caaab68899cc8b7af57e4ec73a6f833f9f9e02fb
Anton <anton380969658858@gmail.com>
2024-02-09 22:10:17 +0200

    rollback tests, fix imports and delete comments

--- user/serializers.py
+++ user/serializers.py
@@ -40,10 +40,6 @@ class AuthTokenSerializer(serializers.Serializer):
                 email=email,
                 password=password
             )
-
-            # The `authenticate` call simply returns None for is_active=False
-            # users. (Assuming the default `ModelBackend` authentication
-            # backend.)
             if not user:
                 msg = "Unable to log in with provided credentials."
                 raise serializers.ValidationError(msg, code="authorization")
