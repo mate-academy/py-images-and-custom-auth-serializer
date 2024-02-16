b80bda5469741db9679b61b6716c5c9418e35121
nschcur <nazae530@gmail.com>
2024-02-16 19:51:24 +0200

    fix solution

--- cinema/models.py
+++ cinema/models.py
@@ -41,8 +41,8 @@ class Actor(models.Model):
 
 def movie_image_file_path(instance, filename):
     _, ext = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}--{uuid.uuid4()}.{ext}"
-    return os.path.join("uploads/images", filename)
+    filename = f"{slugify(instance.title)}--{uuid.uuid4()}{ext}"
+    return os.path.join("uploads", "images", filename)
 
 
 class Movie(models.Model):
