7a04cebeb242f0ea0635a838f1a7a2083d2ac337
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-02-24 17:06:38 +0200

    feat: change image field to read only True in MovieSessionListSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -70,7 +70,7 @@ class MovieSessionSerializer(serializers.ModelSerializer):
 
 class MovieSessionListSerializer(MovieSessionSerializer):
     movie_title = serializers.CharField(source="movie.title", read_only=True)
-    movie_image = serializers.ImageField(source="movie.image")
+    movie_image = serializers.ImageField(source="movie.image", read_only=True)
     cinema_hall_name = serializers.CharField(
         source="cinema_hall.name", read_only=True
     )
