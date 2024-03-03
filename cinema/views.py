58ee588ab4d6c05778e5f7b2be3f3f92223e85c5
LeBwa <lebwa1337@gmail.com>
2024-03-03 09:38:05 +0200

    fix imports and add media to gitignore

--- cinema/views.py
+++ cinema/views.py
@@ -1,9 +1,6 @@
-import os.path
-import uuid
 from datetime import datetime
 
 from django.db.models import F, Count
-from django.utils.text import slugify
 from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
@@ -12,7 +9,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Order
+)
 from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
 
 from cinema.serializers import (
@@ -26,7 +30,8 @@ from cinema.serializers import (
     MovieSessionDetailSerializer,
     MovieListSerializer,
     OrderSerializer,
-    OrderListSerializer, UploadImageSerializer,
+    OrderListSerializer,
+    UploadImageSerializer,
 )
 
 
