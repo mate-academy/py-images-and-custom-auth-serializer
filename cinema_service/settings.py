b2b28a53174444156d431c0a67c4e398ae98ac91
Heorhii Nadiradze <swain.volt@gmail.com>
2024-03-11 12:14:24 +0200

    changes

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -132,16 +132,6 @@ STATIC_URL = "static/"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
-# REST_FRAMEWORK = {
-#     "DEFAULT_PERMISSION_CLASSES": [
-#         "rest_framework.permissions.IsAuthenticated",
-#     ],
-#     "DEFAULT_AUTHENTICATION_CLASSES": [
-#         "rest_framework.authentication.SessionAuthentication",
-#         "rest_framework_simplejwt.authentication.JWTAuthentication",
-#     ],
-# }
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
