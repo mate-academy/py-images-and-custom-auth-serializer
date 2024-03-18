9d40be98e8df398f6906fa9775b72cb64fb1f9d6
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-18 21:28:52 +0200

    change image saving path

--- cinema/models.py
+++ cinema/models.py
@@ -43,7 +43,7 @@ def movie_image_file_path(movie, filename) -> str:
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(movie.title)}-{uuid.uuid4()}.{extension}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads", "images", filename)
 
 
 class Movie(models.Model):
