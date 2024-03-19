4238f27611e2e36569ed597cf67bab5de1d2918e
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-19 11:45:08 +0200

    remove redundant dot in filename

--- cinema/models.py
+++ cinema/models.py
@@ -41,7 +41,7 @@ class Actor(models.Model):
 
 def movie_image_file_path(movie, filename) -> str:
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(movie.title)}-{uuid.uuid4()}.{extension}"
+    filename = f"{slugify(movie.title)}-{uuid.uuid4()}{extension}"
 
     return os.path.join("uploads", "images", filename)
 
