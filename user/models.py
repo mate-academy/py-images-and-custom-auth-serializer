2e4cc8be7072042e352256c316541ab027628972
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-03-20 20:21:28 +0200

    Sol

--- user/models.py
+++ user/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractUser,
     UserManager as DjangoUserManager
 )
