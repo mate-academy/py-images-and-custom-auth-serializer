3c18cb7912192ba9ffb3fe73537934f97d57fa5f
Maksym Operchuk <operlast529@gmail.com>
2024-03-21 16:03:32 +0200

    Solution

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
