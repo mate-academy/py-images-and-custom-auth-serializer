cac60e13abfc4d0b27486584ce72def0460591dc
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-03-21 19:24:35 +0200

    Fixed

--- cinema/models.py
+++ cinema/models.py
@@ -11,20 +11,29 @@ class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
+
     @property
     def capacity(self) -> int:
         return self.rows * self.seats_in_row
+
     def __str__(self):
         return self.name
+
+
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
     def __str__(self):
         return self.name
+
+
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.first_name + " " + self.last_name
+
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -47,25 +56,36 @@ class Movie(models.Model):
 
     class Meta:
         ordering = ["title"]
+
     def __str__(self):
         return self.title
+
+
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+
     class Meta:
         ordering = ["-show_time"]
+
     def __str__(self):
         return self.movie.title + " " + str(self.show_time)
+
+
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
+
     def __str__(self):
         return str(self.created_at)
+
     class Meta:
         ordering = ["-created_at"]
+
+
 class Ticket(models.Model):
     movie_session = models.ForeignKey(
         MovieSession, on_delete=models.CASCADE, related_name="tickets"
@@ -75,6 +95,7 @@ class Ticket(models.Model):
     )
     row = models.IntegerField()
     seat = models.IntegerField()
+
     @staticmethod
     def validate_ticket(row, seat, cinema_hall, error_to_raise):
         for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
@@ -91,6 +112,7 @@ class Ticket(models.Model):
                         f"(1, {count_attrs})"
                     }
                 )
+
     def clean(self):
         Ticket.validate_ticket(
             self.row,
@@ -98,21 +120,24 @@ class Ticket(models.Model):
             self.movie_session.cinema_hall,
             ValidationError,
         )
+
     def save(
-        self,
-        force_insert=False,
-        force_update=False,
-        using=None,
-        update_fields=None,
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
     ):
         self.full_clean()
         return super(Ticket, self).save(
             force_insert, force_update, using, update_fields
         )
+
     def __str__(self):
         return (
             f"{str(self.movie_session)} (row: {self.row}, seat: {self.seat})"
         )
+
     class Meta:
         unique_together = ("movie_session", "row", "seat")
         ordering = ["row", "seat"]
