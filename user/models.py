e1b1d976f94cf90a3d6fd05cf19123e7c7c7916d
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-03-21 18:48:47 +0200

    Model fixed

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-
-
+from django.contrib.auth.models import (
+    AbstractUser,
+    BaseUserManager,
+)
 from django.db import models
 from django.utils.translation import gettext as _
 
