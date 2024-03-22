43cb5b29edbd1968d505ee4fe42cc3959d3060d3
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-03-22 09:14:23 +0200

    Task Solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,6 @@
+import pathlib
+import uuid
+
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
@@ -35,12 +38,19 @@ class Actor(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
+def movie_image_path(instance, filename) -> pathlib.Path:
+    filename = (f"slugify {instance.title}-{uuid.uuid4()}"
+                + pathlib.Path(filename).suffix)
+    return pathlib.Path("uploads/movies/") / pathlib.Path(filename)
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
+    image = models.ImageField(upload_to=movie_image_path, null=True)
 
     class Meta:
         ordering = ["title"]
