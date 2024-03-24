78aac34dbce36be0b5785f60c00c26b7058b02dd
Eugene Protsenko <guitaaarpro@gmail.com>
2024-03-24 13:59:34 +0100

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -110,7 +110,8 @@ class MovieSessionListSerializer(MovieSessionSerializer):
 class TicketSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         data = super(TicketSerializer, self).validate(attrs=attrs)
-        Ticket.validate_ticket(attrs["row"], attrs["seat"], attrs["movie_session"])
+        Ticket.validate_ticket(attrs["row"],
+                               attrs["seat"], attrs["movie_session"])
         return data
 
     class Meta:
