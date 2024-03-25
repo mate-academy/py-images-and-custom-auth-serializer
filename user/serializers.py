ef8687c7774d349ed2dd3d3db2c99e6109db4c3e
AntonSkyrda <mr.skyrda@gmail.com>
2024-03-25 18:06:44 +0200

    Solution

--- user/serializers.py
+++ user/serializers.py
@@ -54,11 +54,15 @@ class AuthTokenSerializer(serializers.Serializer):
 
         if email and password:
             user = authenticate(
-                request=self.context.get("request"), email=email, password=password
+                request=self.context.get("request"),
+                email=email,
+                password=password
             )
 
             if not user:
-                msg = gettext("Unable to authenticate with provided credentials.")
+                msg = gettext(
+                    "Unable to authenticate with provided credentials."
+                )
                 raise serializers.ValidationError(msg, code="authentication")
 
         else:
